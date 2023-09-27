aeff1e85febcd3ecd69c29fa2d8612736a81a909
PedamalluAbhiram <ugs207145_it.abhiram@cbit.org.in>
2023-09-27 11:09:44 +0530

    added_song

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Welcome to Spotify")
+console.log("Welcome to Spotify");
+//Initialise the variables 
+let songIndex=0;
+let audioElement = new Audio('1.mp3');
+let masterPlay= document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let songs =[
+    {songName:"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
+]
+//audioElement.play();
+
+//Handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+    }
+})
+//Listen to Events
+myProgressBar.addEventListener('timeupdate',()=>{
+    console.log('timeupdate');
+
+})
+
